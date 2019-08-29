d98040ea375355f20b4fd9446b6b60aa0cb3aaf5
Alexandr Martynov <alex@magemore.com>
2019-08-29 06:42:23 +0000

    home content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,14 @@
 @section('title', 'Home')
 @section('content')
 <div class="container">
-    <main class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
-        <div class="title">Home Page</div>
-        <div class="quote">Our Home page!</div>
-    </main>
+    <div class="row banner">
+        <main class="col-12" role="main">
+            <h1 class="text-center margin-top-100 editContent">Learning Laravel 5</h1>
+            <h3 class="text-center margin-top-100 editContent">Building Practical Applications</h3>
+            <div class="text-center">
+                <img src="http://learninglaravel.net/img/LearningLaravel5_cover0.png" width="302" height="391" alt="">
+            </div>
+        </main>
+    </div>
 </div>
 @endsection
